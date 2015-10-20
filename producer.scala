6eb3dffabe2e1f70907657c8b537c7b2658e9ed2
philchute <philchute@gmail.com>
2015-10-20 15:56:31 +0000

    updated producer to be simpler

--- producer.scala
+++ producer.scala
@@ -4,61 +4,73 @@ import java.net.ServerSocket
 //import java.util.Date
 import scala.util.Random
 import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
 //import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 object producer{
         def main(args: Array[String]){
+                val conf = new SparkConf().setMaster("local").setAppName("producer")
+                val sc = new SparkContext(conf)
                 //Create random number generator
                 val random = new Random()
                 //Maximum number of events per second
                 val MaxEvents = 100
                 //create object of data
                 val lines = sc.textFile("/kddcup.data_10_percent")
-                //val linesOfValues = scala.io.Source.fromInputStream(namesResource).getLines().toList.head.split("\n").toSeq
-
                 def generateNetworkEvents(n:Int) = {
+                  (1 to n).map{i =>
                    val line = lines.takeSample(true, 1, random.nextLong)
-                   val duration = line.next()
-                   val protocol_type = line.next()
-                   val service = line.next()
-                   val flag = line.next()
-                   val src_bytes = line.next()
-                   val dst_bytes = line.nextInt()
-                   val land = line.next()
-                   val wrong_fragment = line.nextInt()
-                   val urgent = line.nextInt()
-                   val hot = line.nextInt()
-                   val num_failed_logins = line.nextInt()
-                   val logged_in = line.next()
-                   val num_compromised = line.nextInt()
-                   val root_shell = line.nextInt()
-                   val su_attempted = line.nextInt()
-                   val num_root = line.nextInt()
-                   val num_file_creations = line.nextInt()
-                   val num_shells = line.nextInt()
-                   val num_access_files = line.nextInt()
-                   val num_outbound_cmds = line.nextInt()
-                   val is_host_login = line.next()
-                   val is_guest_login = line.next()
-                   val count = line.nextInt()
-                   val srv_count = line.nextInt()
-                   val serror_rate = line.nextInt()
-                   val srv_serror_rate = line.nextInt()
-                   val rerror_rate = line.nextInt()
-                   val srv_rerror_rate = line.nextInt()
-                   val same_srv_rate = line.nextInt()
-                   val diff_srv_rate = line.nextInt()
-                   val srv_diff_host_rate = line.nextInt()
-                   val dst_host_count = line.nextInt()
-                   val dst_host_srv_count = line.nextInt()
-                   val dst_host_same_srv_rate = line.nextInt()
-                   val dst_host_diff_srv_rate = line.nextInt()
-                   val dst_host_same_src_port_rate = line.nextInt()
-                   val dst_host_srv_diff_host_rate = line.nextInt()
-                   val dst_host_serror_rate = line.nextInt()
-                   val dst_host_srv_serror_rate = line.nextInt()
-                   val dst_host_rerror_rate = line.nextInt()
-                }
+                   /*val duration = line(0)
+                   val protocol_type = line(1)
+                   val service = line(2)
+                   val flag = line(3)
+                   val src_bytes = line(4)
+                   val dst_bytes = line(5)
+                   val land = line(6)
+                   val wrong_fragment = line(7)
+                   val urgent = line(8)
+                   val hot = line(9)
+                   val num_failed_logins = line(10)
+                   val logged_in = line(11)
+                   val num_compromised = line(12)
+                   val root_shell = line(13)
+                   val su_attempted = line(14)
+                   val num_root = line(15)
+                   val num_file_creations = line(16)
+                   val num_shells = line(17)
+                   val num_access_files = line(18)
+                   val num_outbound_cmds = line(19)
+                   val is_host_login = line(20)
+                   val is_guest_login = line(21)
+                   val count = line(22)
+                   val srv_count = line(23)
+                   val serror_rate = line(24)
+                   val srv_serror_rate = line(25)
+                   val rerror_rate = line(26)
+                   val srv_rerror_rate = line(27)
+                   val same_srv_rate = line(28)
+                   val diff_srv_rate = line(29)
+                   val srv_diff_host_rate = line(30)
+                   val dst_host_count = line(31)
+                   val dst_host_srv_count = line(32)
+                   val dst_host_same_srv_rate = line(33)
+                   val dst_host_diff_srv_rate = line(34)
+                   val dst_host_same_src_port_rate = line(35)
+                   val dst_host_srv_diff_host_rate = line(36)
+                   val dst_host_serror_rate = line(37)
+                   val dst_host_srv_serror_rate = line(38)
+                   val dst_host_rerror_rate = line(39)
+                   (duration, protocol_type, service, flag, src_bytes, dst_bytes, land, wrong_fragment, 
+                        urgent, hot, num_failed_logins, logged_in, num_compromised, root_shell, su_attempted, 
+                        num_root, num_file_creations, num_shells, num_access_files, num_outbound_cmds, is_host_login, 
+                        is_guest_login, count, srv_count, serror_rate, srv_serror_rate, rerror_rate, srv_rerror_rate, 
+                        same_srv_rate, diff_srv_rate, srv_diff_host_rate, dst_host_count, dst_host_srv_count, 
+                        dst_host_same_srv_rate, dst_host_diff_srv_rate, dst_host_same_src_port_rate, dst_host_srv_diff_host_rate, 
+                        dst_host_serror_rate, dst_host_srv_serror_rate, dst_host_rerror_rate, dst_host_srv_rerror_rate)
+                  */
+                  (line)
+                  }
+                }  
                 //create a network producer
                 val listener = new ServerSocket(9999)
                 println("Listening on port: 9999")
@@ -75,7 +87,8 @@ object producer{
                                                 val networkEvents = generateNetworkEvents(num)
                                                 networkEvents.foreach { event =>
                                                         //productIterator convert tuple into iterator
-                                                        out.write(event.productIterator.mkString(","))
+                                                        //out.write(event.productIterator.mkString(","))
+                                                        out.write(event.mkString(","))
                                                         out.write("\n")
                                                 }
                                                 out.flush()
